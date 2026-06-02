524ed668141a599962dad58e3026c4cb1e479fea
trvlas96 <trvlasova96@gmail.com>
2026-06-03 01:33:04 +0500

    Fix all game and image paths

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Art-Logos/">
       <App />
     </BrowserRouter>
   </StrictMode>,
